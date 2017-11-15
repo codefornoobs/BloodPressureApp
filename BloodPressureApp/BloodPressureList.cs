922f2c12d19a8e41a89d4f6a95b0aa9f53bc3429
Diogo Cró <diogocror@gmail.com>
2017-11-15 11:38:24 +0000

    BPList Activity

---
+++ BloodPressureApp/BloodPressureList.cs
@@ -0,0 +1,17 @@
+﻿
+using Android.App;
+using Android.OS;
+
+namespace BloodPressureApp
+{
+    [Activity(Label = "BloodPressureList")]
+    public class BloodPressureList : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.BloodPressureList);
+        }
+    }
+}
