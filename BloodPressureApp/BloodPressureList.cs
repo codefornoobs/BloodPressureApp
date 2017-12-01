341f40126ab732d41b299a44079440c915de9f6e
Diogo Cró <diogocror@gmail.com>
2017-12-01 18:22:38 +0000

    listing all measures taken

--- BloodPressureApp/BloodPressureList.cs
+++ BloodPressureApp/BloodPressureList.cs
@@ -1,17 +1,46 @@
 ﻿
 using Android.App;
 using Android.OS;
+using Android.Widget;
+using SQLite;
+using System;
+using System.IO;
+using Environment = System.Environment;
 
 namespace BloodPressureApp
 {
     [Activity(Label = "BloodPressureList")]
     public class BloodPressureList : Activity
     {
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.BloodPressureList);
+
+            TextView txtBloodList = FindViewById<TextView>(Resource.Id.textView1);
+
+            txtBloodList.Text = RetrieveBloodPressureMeasures();
+
+        }
+
+        private string RetrieveBloodPressureMeasures()
+        {
+            var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "BPdatabase.db3");
+
+
+            var db = new SQLiteConnection(dbPath);
+            var table = db.Table<BloodPressureMeasurement>();
+            string result = string.Empty;
+
+            foreach (BloodPressureMeasurement item in table)
+            {
+                result += item + "\n";
+            }
+
+            Console.WriteLine(result);
+            return result;
         }
     }
 }
---
+++ BloodPressureApp/BloodPressureMeasurement.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+using System;
+
+namespace BloodPressureApp
+{
+    class BloodPressureMeasurement
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        public string HighValue { get; set; }
+
+        public string LowValue { get; set; }
+        public DateTime InsertDate { get; set; }
+
+        public BloodPressureMeasurement()
+        {
+
+        }
+        public override string ToString()
+        {
+            return string.Format("High Value={0}, Low Value={1}, Inserted Date={2}",
+                 HighValue, LowValue, InsertDate);
+        }
+    }
+}
